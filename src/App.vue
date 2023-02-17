096c928babe374df9948b3929d8cf4de21fa597c
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2023-02-17 00:28:19 -0600

    frontend should be finished. I have a place for all my data to land basically

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div>
+      <MainCard />
+    
+
+    </div>
+  </header>
+</template> 
+
+<script>
+
+// import ProjectsCard from './components/ProjectsCard.vue'
+import MainCard from "./components/MainCard.vue"
+///above are imports
+
+
+export default {
+  name: "App",
+  components: {
+
+    MainCard,
+    
+  }
+}
+
+</script>
+
+
+<style>
+#app{
+  font-family: Avenir, Helvetic, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+
+}
+
+
+</style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="main-container">
+        
+
+        <div class="container-one">
+            <div class="profile">
+                <img src="../assets/profilepicture.jpeg" alt="">
+                <h1>Porfolio</h1> 
+                <h1>Claude Allen</h1>
+                <p>Software Dev and Reef Keeper</p>
+            </div>
+            <div class="social-links">
+                <div class="link">
+                        <a href="">
+                            <h2>Github</h2>
+                            <img src="../assets/github.png" alt="">
+                        </a>
+                    </div>
+                    <div class="link">
+                        <a href="">
+                            <h2>LinkedIn</h2>
+                            <img src="../assets/linkedin.png" alt="">
+                        </a>
+                    </div>
+
+                    <div class="link">
+                        <a href="">
+                            <h2>Instagram</h2>
+                            <img src="../assets/instagram-icon-small.jpeg" alt="">
+                        </a>
+                        
+                    </div>
+            </div>
+        </div>
+        <div class="container-two">
+            <div class="aboutme">
+                <h1>About Me</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit
+                    . Quod tenetur ex itaque qui consequatur ab incidunt adipisci 
+                    illo autem culpa? Similique repellendus ratione, aperiam maxime 
+                    error neque tempora ex quia!</p>
+            </div>
+            <div class="technology-section">
+                <h1>Technologies Studiedd</h1>
+                  <div class="logos">
+                    <img src="../assets/javascripticon.png" alt="">
+                    <img src="../assets/python.png" alt="">
+                    <img src="../assets/node_expresslogo.jpeg" alt="">
+
+                </div>
+            </div>
+            <div class="button-container">
+                <div class="project-buttons">
+                    <a href="">My Projects</a>
+                    
+                </div>
+                <div class="project-buttons">
+                    <a href="">My Resume</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    
+}
+
+</script>
+
+
+
+<style scoped>
+ img{
+    height: 20px;
+ }
+
+
+  .main-container {
+        display: flex;
+        flex-direction: row;
+        background-color: #ac179b;
+        border-radius: 25px;
+
+    } 
+
+    .container-one{
+        background-color: #58b533;
+        padding: 2.5%;
+        width: 40%;
+        border-radius: 25px;
+
+    }
+
+    .container-two{
+        background-color: purple;
+        width: 60%;
+
+    }
+
+    .profile img{
+        height: 5rem;
+        border-radius: 50%;
+
+    }
+
+    .link a{
+        text-decoration: none;
+        color: black;
+        font-size: 12px; 
+    }
+    
+    .link img{
+        height: 2rem;
+    }
+
+    .aboutme{
+        text-align: left;
+        margin: 2.5%;
+        width:70%
+    }
+
+    .logos img{
+        height: 2.5rem;
+
+    }
+
+
+    .button-container{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .projects-button{
+        background-color: #ac179b;
+        margin: 5px;
+        padding: 5px;
+        border-radius: 15px;
+        
+    }
+
+    .projects-button a{
+        text-decoration: none;
+        color: grey;
+    }
+
+
+</style>
